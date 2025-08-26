512cb574729e613e6cc2318c21fc7eab3fed001e
Lorenzo Cleti <cleti.lorenzo@gmail.com>
2025-08-26 17:31:20 +0200

    ADDED levelUp Data

--- src/aetherlum_survivor/util/LevelUpData.java
+++ src/aetherlum_survivor/util/LevelUpData.java
@@ -2,8 +2,6 @@ package aetherlum_survivor.util;
 
 import java.util.Map;
 
-import aetherlum_survivor.util.EntityData.EntityStats;
-
 public class LevelUpData {
 
     //! GENERIC VALUE
@@ -37,23 +35,48 @@ public class LevelUpData {
         }
     }
 
-    //id_code come key
+    // FV = incresed with flat value || PV = increased with percentage value
+    public static final int CODE_SPD_FV = 10;
+    public static final double SPD_FV = 1;
+    public static final String SPD_FV_DESCR = "SPEED: +" + SPD_FV;
 
-    /* 
-        this.speed = EntityData.PLAYER_SPD;
-        this.maxHitPoints = EntityData.PLAYER_MAX_HP;
-        this.currentHP = this.maxHitPoints;
-        this.damage = EntityData.PLAYER_DMG;
-        this.damageResistance = EntityData.PLAYER_DMG_RST;
-        this.fireRate = EntityData.PLAYER_FIRE_RATE;
-        EntityData.BASE_PROJ_TYPE
-    */
+    public static final int CODE_MAX_HP_FV = 20;
+    public static final double MAX_HP_FV = 100;
+    public static final String MAX_HP_FV_DESCR = "MAX HP: +" + MAX_HP_FV; 
 
-    /* 
+    public static final int CODE_MAX_HP_PV = 25;
+    public static final double MAX_HP_PV = 1.1;
+    public static final String MAX_HP_PV_DESCR = "MAX HP: +" + (MAX_HP_PV - 1)*100 + "%";
+
+    public static final int CODE_DMG_FV = 50;
+    public static final double DMG_FV = 1;
+    public static final String DMG_FV_DESCR = "DAMAGE: +" + DMG_FV; 
+
+    public static final int CODE_DMG_RST_FV = 55;
+    public static final double DMG_RST_FV = 0.05;
+    public static final String DMG_RST_FV_DESCR = "DAMAGE RESISTANCE (%): +" + DMG_RST_FV;
+
+    public static final int CODE_FIRE_RATE_PV = 70;
+    public static final double FIRE_RATE_PV = 0.97;
+    public static final String FIRE_RATE_PV_DESCR = "FIRE RATE: -" + (1 - FIRE_RATE_PV) + "%";
+
+    public static final int CODE_UNLOCK_PIERCING_PROJ = 100;
+    public static final int PRCNG_PROJ_TYPE = EntityData.PIERCING_PROJ_TYPE;
+    public static final String UNLOCK_PRCNG_PROJ_DESCR = "UNLOCKS PIERCING SPELL";
+
+    public static final int CODE_UNLOCK_FAST_PROJ = 110;
+    public static final int FST_PROJ_TYPE = EntityData.FAST_PROJ_TYPE;
+    public static final String UNLOCK_FST_PROJ_DESCR = "UNLOCKS FAST SPELL";
+
     //! STATS OF ALL LEVELUP POSSIBILITIES mapped with the type as the key
-    public static final Map<Integer, LevelUpData> STATS = Map.of(
-        BASE_PROJ_TYPE, new EntityStats(BASE_PROJ_WIDTH, BASE_PROJ_HEIGHT, BASE_PROJ_SPD, BASE_PROJ_MAX_HP, NOT_USEFUL_VALUE, NOT_USEFUL_VALUE, BASE_PROJ_SPRITE_PATH, BASE_PROJ_RATE_MOD, BASE_PROJ_DMG_MOD),
+    public static final Map<Integer, LevelUpOptions> STATS = Map.of(
+        CODE_SPD_FV, new LevelUpOptions(SPD_FV_DESCR, SPD_FV, NULL_VALUE),
+        CODE_MAX_HP_FV, new LevelUpOptions(MAX_HP_FV_DESCR, MAX_HP_FV, NULL_VALUE),
+        CODE_MAX_HP_PV, new LevelUpOptions(MAX_HP_PV_DESCR, NULL_VALUE, MAX_HP_PV),
+        CODE_DMG_FV, new LevelUpOptions(DMG_FV_DESCR, DMG_FV, NULL_VALUE),
+        CODE_DMG_RST_FV, new LevelUpOptions(DMG_RST_FV_DESCR, DMG_RST_FV, NULL_VALUE),
+        CODE_FIRE_RATE_PV, new LevelUpOptions(FIRE_RATE_PV_DESCR, FIRE_RATE_PV, NULL_VALUE),
+        CODE_UNLOCK_PIERCING_PROJ, new LevelUpOptions(UNLOCK_PRCNG_PROJ_DESCR, PRCNG_PROJ_TYPE),
+        CODE_UNLOCK_FAST_PROJ, new LevelUpOptions(UNLOCK_FST_PROJ_DESCR, FST_PROJ_TYPE)
     );
-    */
-    
 }
